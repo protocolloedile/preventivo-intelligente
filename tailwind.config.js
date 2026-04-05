72025ad8baa7c2111d1f1227e7c54e6c027710f9
protocolloedile <protocolloedile@gmail.com>
2026-04-05 08:17:27 +0200

    Add orange color safelist to Tailwind config for PricingPage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+          "./index.html",
+          "./src/**/*.{js,ts,jsx,tsx}",
+        ],
+    safelist: [
+          'bg-orange-50',
+          'bg-orange-500',
+          'bg-orange-600',
+          'border-orange-300',
+          'border-orange-500',
+          'text-orange-600',
+          'text-orange-700',
+          'ring-orange-200',
+          'ring-orange-500',
+          'hover:bg-orange-600',
+          'hover:border-orange-300',
+          'focus:ring-orange-500',
+        ],
+    theme: {
+          extend: {},
+    },
+    plugins: [],
 }
